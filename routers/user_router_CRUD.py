ec1bdc0c3f149b9b60094472108525e84a4726f6
Salma Salah <Salmahemeda1@gmail.com>
2026-04-06 21:37:06 +0200

    Update user_router_CRUD.py

--- routers/user_router_CRUD.py
+++ routers/user_router_CRUD.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from utils.jwt_handler import verify_jwt
 
 router = APIRouter(prefix="/users", tags=["Users"])
+security = HTTPBearer()  # للتحقق من Authorization header
 
-def get_current_user(token: str):
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
     payload = verify_jwt(token)
     if not payload:
         raise HTTPException(401, "Invalid or expired token")
     return payload
 
+# endpoint /me
 @router.get("/me")
 def read_me(current_user: dict = Depends(get_current_user)):
     return current_user
