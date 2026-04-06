651248538f8b84a0b7f46a9eaacb3278096a77fd
Salma Salah <Salmahemeda1@gmail.com>
2026-04-06 21:36:15 +0200

    Update register.py

--- routers/register.py
+++ routers/register.py
@@ -1,21 +1,27 @@
+from fastapi import APIRouter, HTTPException
+from models.user_model import UserRegister
+from passlib.hash import bcrypt
+from database import get_connection
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
 @router.post("/register")
 def register(user: UserRegister):
+    conn = get_connection()
+    cursor = conn.cursor()
 
-    df = read_users()
-
-    if user.email in df["email"].values:
+    
+    cursor.execute("SELECT * FROM users WHERE email=?", (user.email,))
+    if cursor.fetchone():
         raise HTTPException(400, "Email already exists")
 
-    new_id = 1 if df.empty else int(df["id"].max()) + 1
+    hashed_password = bcrypt.hash(user.password)
 
-    new_user = {
-        "id": new_id,
-        "email": user.email,
-        "password": user.password,  # هنشفرها بعد كده
-        "role": "customer"
-    }
-
-    df = df._append(new_user, ignore_index=True)
-    save_users(df)
+    cursor.execute(
+        "INSERT INTO users (email, password, role) VALUES (?, ?, ?)",
+        (user.email, hashed_password, "customer")
+    )
+    conn.commit()
+    conn.close()
 
     return {"message": "User created"}
