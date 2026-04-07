4d642cc7f49741e729b9f4a59c431790b2eb9b6c
Nada Walied <nadawaliedsamieh16522@gmail.com>
2026-04-08 01:29:18 +0200

    Add stock field to Product models
    Added 'stock' field to ProductCreate and ProductResponse models with validation.

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,15 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 class ProductCreate(BaseModel):
     name: str
     category_id: int
+    stock: int = Field(..., ge=0)   # ✅ MUST be >= 0
 
 
 class ProductResponse(BaseModel):
     id: int
     name: str
     category_id: Optional[int] = None
+    stock: int   # ✅ ADD THIS
+
 
     class Config:
         from_attributes = True
